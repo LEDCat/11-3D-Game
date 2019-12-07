a1da0975dc776a944a83595890275713a216b943
LEDCat <lucaed64@gmail.com>
2019-12-07 00:08:02 -0500

    Updated

--- DEMO/Scripts/FPSCharacter.gd
+++ DEMO/Scripts/FPSCharacter.gd
@@ -3,7 +3,7 @@ extends KinematicBody
 onready var camera = $Pivot/Camera
 var gravity = -30
 var jump_speed = 12
-var max_speed = 8
+var max_speed = 3
 var mouse_sensitivity = 0.002 #radians/pixel
 
 var velocity = Vector3()
